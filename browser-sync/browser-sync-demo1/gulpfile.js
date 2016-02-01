ef601b597e19f40984efa2a92238fafa933b65b0
yanlbo <yanlbo@sina.com>
2016-02-01 15:55:32 +0800

    browser-sync的示例
    很好很强大，可以代替serve-static

---
+++ browser-sync/browser-sync-demo1/gulpfile.js
@@ -0,0 +1,32 @@
+var gulp = require('gulp');
+var browserSync = require('browser-sync').create();
+var del = require('del');
+
+gulp.task('server',['views','static'],function(){
+  browserSync.init({
+    server:{
+      baseDir:'build'
+    }
+  });
+  //监听变化并reload
+  gulp.watch('static/css/**',['static']).on('change',browserSync.reload);
+  gulp.watch('views/*.html',['views']).on('change',browserSync.reload);
+});
+
+gulp.task('del',function(){
+  del.sync(['build']);
+});
+
+gulp.task('static',function(){
+  return gulp.src('static/css/*.css')
+  .pipe(gulp.dest('build/static/css'))
+  .pipe(browserSync.stream());
+});
+
+gulp.task('views',function(){
+  return gulp.src('views/**')
+  .pipe(gulp.dest('build/views'))
+  .pipe(browserSync.stream());//加入到BS流中
+});
+
+gulp.task('default',['server']);
