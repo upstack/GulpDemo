68efb514927c06149425dc6786fb7e9a78021f84
yanlbo <yanlbo@sina.com>
2016-02-03 17:06:58 +0800

    将雪碧图样式文件合并到其他css文件中，任务串行调用
    1、构建过程中创建了临时文件，构建完毕后，删除临时文件
    2、串行：利用gulp.start函数的回调进行的串行任务调用，gulp.task中依赖项执行顺序并不十分准确

--- gulp.spritesmith/gulp.spritesmith-demo1/gulpfile.js
+++ gulp.spritesmith/gulp.spritesmith-demo1/gulpfile.js
@@ -7,18 +7,29 @@ var csso = require('gulp-csso');
 var imagemin = require('gulp-imagemin');
 var merge = require('merge-stream'); //合并多个stream
 
+var concat = require('gulp-concat');
 
-gulp.task('del', function() {
-  del.sync(['build']);
+/*
+删除
+ */
+gulp.task('delBuild', function() {
+  del.sync('build');
 });
 
+gulp.task('delTemp',function(){
+  del.sync('temp');
+});
+
+/*
+产出雪碧图
+ */
 gulp.task('sprites', function() {
   var spriteData = gulp.src('static/img/*.png')
     .pipe(spritesmith({
       imgName: 'sprites.png',
       cssName: 'sprites.css',
-      imgPath: 'static/img/sprites.png',
-      cssTemplate: 'static/tmpl/_sprites.handlebars'
+      imgPath: 'static/img/sprites.png',//css中图片的路径
+      cssTemplate: 'static/tmpl/_sprites.handlebars'//css模板的路径
     }));
 
   //直接输出到一个目录下
@@ -31,8 +42,36 @@ gulp.task('sprites', function() {
     .pipe(gulp.dest('build/static/img'));
 
   var cssStream = spriteData.css
-    // .pipe(csso())
-    .pipe(gulp.dest('build/static/css'));
+    .pipe(gulp.dest('temp/static/css'));
 
   return merge(imgStream, cssStream);
+});
+
+/*
+将雪碧图进行
+ */
+gulp.task('concat',function(){
+  return gulp.src(['static/css/*.css','temp/static/css/sprites.css'])
+  .pipe(concat('main.css'))
+  .pipe(csso())//压缩
+  .pipe(gulp.dest('build/static/css'));
+});
+
+/*
+任务执行队列（利用回调函数，依赖同步执行，略显杂乱，希望有更好的方式，像是状态机）
+ */
+gulp.task('taskQueue',function(){
+  gulp.start(['delBuild','delTemp'],function(){
+    console.log('=== del end ===');
+    gulp.start('sprites',function(){
+      console.log('=== sprites end ===');
+      gulp.start('concat',function(){
+        console.log('=== concat end ===');
+        //重复调用任务一定要复写回调，不然将继续执行上一次定义的回调
+        gulp.start('delTemp',function(){
+          console.log('=== delTemp end ===');
+        });
+      });
+    });
+  });
 });
